31c07a3cef0a7ad27cb1a0ddbfcde8ed39276c94
kvashish98 <56497050+kvashish98@users.noreply.github.com>
2021-10-10 23:22:36 -0500

    Add files via upload

---
+++ Pair.java
@@ -0,0 +1,21 @@
+public class Pair<T>
+{
+    public T getV1()
+    { 
+        return v1; 
+    }
+    public void setV1(T s)
+    { 
+        v1 = s; 
+    }
+    public T getV2()
+    { 
+        return v2; 
+    }
+    public void setV2(T s)
+    { 
+        v2 = s; 
+    }
+    private T v1;
+    private T v2;
+}
